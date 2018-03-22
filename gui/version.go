fe96dcb1b5f55a8ae45ebe024e5c92d98689a070
Darshil Chanpura <dtchanpura@gmail.com>
2018-03-23 00:40:55 +0530

    config: added cors configuration in config.go

--- gui/version.go
+++ gui/version.go
@@ -1,4 +1,4 @@
 package gui
 
 // VersionEpoch for last gui update time.
-var VersionEpoch = 1521350646
+var VersionEpoch = 1521745855
--- server/config.go
+++ server/config.go
@@ -2,9 +2,18 @@ package server
 
 // Configuration for storing configuration
 type config struct {
-	Server   string `json:"server"`
-	Database string `json:"database"`
-	Host     string `json:"host"`
-	Port     int    `json:"port"`
-	GUIPath  string `json:"guiPath,omitempty"`
+	Server   string             `json:"server"`
+	Database string             `json:"database"`
+	Host     string             `json:"host"`
+	Port     int                `json:"port"`
+	GUIPath  string             `json:"guiPath,omitempty"`
+	CORS     *CORSConfiguration `json:"cors,omitempty"`
+}
+
+// CORSConfiguration for storing CORS Configura
+type CORSConfiguration struct {
+	Enable         bool     `json:"enable,omitempty"`
+	AllowedHeaders []string `json:"allowedHeaders,omitempty"`
+	AllowedOrigins []string `json:"allowedOrigins,omitempty"`
+	AllowedMethods []string `json:"allowedMethods,omitempty"`
 }
